4ac186c3bd2408834356fde40e9740705ad0a05d
Yevgeniy <leonov-y@gmail.com>
2023-05-10 17:14:05 +0600

    initial commit

---
+++ backend/api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    city = models.CharField(max_length=100)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return self.name
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    salary = models.FloatField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
